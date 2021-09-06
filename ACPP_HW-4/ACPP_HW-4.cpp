65fa3d955e77eb8d1f3e77421acdc903db775db3
Macilnor <macilnor@mail.ru>
2021-09-06 12:10:26 +0300

    Задание 1

--- ACPP_HW-4/ACPP_HW-4.cpp
+++ ACPP_HW-4/ACPP_HW-4.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "insert_sorted.h"
 
 int main()
 {
+	std::vector<int> vect(25);
+	std::generate(vect.begin(), vect.end(), []() {return rand() % 100; });
+	std::sort(vect.begin(), vect.end());
 
+	std::copy(vect.begin(), vect.end(), std::ostream_iterator<int>{std::cout, " "});
+	std::cout << std::endl;
+
+	insert_sorted<std::vector<int>, int>(vect, 5);
+	insert_sorted<std::vector<int>, int>(vect, 35);
+	insert_sorted<std::vector<int>, int>(vect, 99);
+
+	std::copy(vect.begin(), vect.end(), std::ostream_iterator<int>{std::cout, " "});
+	std::cout << std::endl;
 }
 
